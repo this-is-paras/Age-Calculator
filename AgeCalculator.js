0900d0b125575e4b1ea8bce7a8b9d27cc899056a
Paras <pkaittan123@gmail.com>
2025-02-28 12:18:09 +0530

    initial commit

--- AgeCalculator.js
+++ AgeCalculator.js
@@ -8,18 +8,20 @@ calculatebtn.addEventListener("click", () =>{
     }else {
         console.log("input", inputbtn.value);
         const dob = new Date(inputbtn.value);
+        
         console.log("dob",dob);
         const dob_year = dob.getFullYear();
         console.log("dob_year",dob_year);
 
         const now = new Date();
         console.log("now",now);
+
         const now_year = now.getFullYear();
         console.log("now_year",now_year);
+
         const age = now_year - dob_year;
         console.log("age",age);
 
         ageresult.innerHTML = `Your age is ${age}`;
-
     }
 })
