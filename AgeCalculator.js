75d1c3600d7e07689a4ca2d68c01a51eb54121a8
Paras <pkaittan123@gmail.com>
2025-03-04 19:01:43 +0530

    initial commit

--- AgeCalculator.js
+++ AgeCalculator.js
@@ -1,10 +1,11 @@
-const inputbtn = document.querySelector(".age-dob-input")
-const calculatebtn = document.querySelector(".calculate-btn")
-const ageresult = document.querySelector(".age-result")
+const inputbtn = document.querySelector(".age-dob-input");
+const calculatebtn = document.querySelector(".calculate-btn");
+const ageresult = document.querySelector(".age-result");
 
 calculatebtn.addEventListener("click", () =>{
     if(inputbtn.value === ""){
         alert("Please enter your date of birth");
+
     }else {
         console.log("input", inputbtn.value);
         const dob = new Date(inputbtn.value);
