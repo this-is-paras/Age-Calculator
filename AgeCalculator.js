f0ca75efceb1bb47d32b1b29cf0e32e491f3be21
Paras <pkaittan123@gmail.com>
2025-03-19 21:26:21 +0530

    initial commit

--- AgeCalculator.js
+++ AgeCalculator.js
@@ -1,26 +1,26 @@
- const inputbtn = document.querySelector(".age-dob-input");   
- const calculatebtn = document.querySelector(".calculate-btn");   
- const ageresult = document.querySelector(".age-result"); 
- 
-calculatebtn.addEventListener("click", () =>{ 
-    if(inputbtn.value === ""){ 
-        alert("Please enter your date of birth");  
-    }else {  
-        console.log("input", inputbtn.value);  
-        const dob = new Date(inputbtn.value); 
- 
-        console.log("dob",dob);
-        const dob_year = dob.getFullYear();  
+const inputbtn = document.querySelector(".age-dob-input");
+const calculatebtn = document.querySelector(".calculate-btn");
+const ageresult = document.querySelector(".age-result");
 
-        console.log("dob_year",dob_year);     
-        const now = new Date();   
+calculatebtn.addEventListener("click", () => {
+    if (inputbtn.value === "") {
+        alert("Please enter your date of birth");
+    } else {
+        console.log("input", inputbtn.value);
+        const dob = new Date(inputbtn.value);
 
-        console.log("now",now);   
-        const now_year = now.getFullYear();   
-        console.log("now_year",now_year);   
-        const age = now_year - dob_year;   
+        console.log("dob", dob);
+        const dob_year = dob.getFullYear();
 
-        console.log("age",age);  
-        ageresult.innerHTML = `Your age is ${age}`;   
-    }   
+        console.log("dob_year", dob_year);
+        const now = new Date();
+
+        console.log("now", now);
+        const now_year = now.getFullYear();
+        console.log("now_year", now_year);
+        const age = now_year - dob_year;
+
+        console.log("age", age);
+        ageresult.innerHTML = `Your age is ${age}`;
+    }
 })    
